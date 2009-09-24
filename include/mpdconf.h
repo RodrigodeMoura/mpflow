53de3cb099887e5661d2f896b6e9c7e8406286d1
walter <walter@d90a1141-1c03-0410-82d0-813645769019>
2009-09-24 19:47:07 +0000

    added sources
    git-svn-id: file:///Users/walter/linux/walter/src/svnroot/trunk/mpflow@3219 d90a1141-1c03-0410-82d0-813645769019

---
+++ include/mpdconf.h
@@ -0,0 +1,12 @@
+/*
+	mpdconf.h	WJ109
+*/
+
+#ifndef MPDCONF_H_WJ109
+#define MPDCONF_H_WJ109	1
+
+
+
+#endif	/* MPDCONF_H_WJ109 */
+
+/* EOB */
---
+++ mpdconf.c
@@ -0,0 +1,12 @@
+/*
+	mpdconf.c	WJ109
+*/
+
+#include "mpdconf.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/* EOB */
