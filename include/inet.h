6ce3dd6b7f570b6c33bd22a92be2090564279bfa
walter <walter@d90a1141-1c03-0410-82d0-813645769019>
2009-09-24 20:44:25 +0000

    internet code
    git-svn-id: file:///Users/walter/linux/walter/src/svnroot/trunk/mpflow@3222 d90a1141-1c03-0410-82d0-813645769019

---
+++ include/inet.h
@@ -0,0 +1,12 @@
+/*
+	inet.h	WJ109
+*/
+
+#ifndef INET_H_WJ109
+#define INET_H_WJ109	1
+
+
+
+#endif	/* INET_H_WJ109 */
+
+/* EOB */
---
+++ inet.c
@@ -0,0 +1,12 @@
+/*
+	inet.c	WJ109
+*/
+
+#include "inet.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/* EOB */
