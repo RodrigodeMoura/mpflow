91c5702263beb9808bc102048b157033c6ba27fd
walter <walter@d90a1141-1c03-0410-82d0-813645769019>
2009-09-25 14:17:45 +0000

    added texture source
    git-svn-id: file:///Users/walter/linux/walter/src/svnroot/trunk/mpflow@3227 d90a1141-1c03-0410-82d0-813645769019

---
+++ include/texture.h
@@ -0,0 +1,12 @@
+/*
+	texture.h	WJ109
+*/
+
+#ifndef TEXTURE_H_WJ109
+#define TEXTURE_H_WJ109	1
+
+
+
+#endif	/* TEXTURE_H_WJ109 */
+
+/* EOB */
---
+++ texture.c
@@ -0,0 +1,12 @@
+/*
+	texture.c	WJ109
+*/
+
+#include "texture.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/* EOB */
