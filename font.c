97264e425375e828eccf55405d48faec905e3fed
walter <walter@d90a1141-1c03-0410-82d0-813645769019>
2009-09-26 15:34:39 +0000

    added font sources
    git-svn-id: file:///Users/walter/linux/walter/src/svnroot/trunk/mpflow@3243 d90a1141-1c03-0410-82d0-813645769019

---
+++ font.c
@@ -0,0 +1,12 @@
+/*
+	font.c	WJ109
+*/
+
+#include "font.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/* EOB */
---
+++ include/font.h
@@ -0,0 +1,12 @@
+/*
+	font.h	WJ109
+*/
+
+#ifndef FONT_H_WJ109
+#define FONT_H_WJ109	1
+
+
+
+#endif	/* FONT_H_WJ109 */
+
+/* EOB */
