3c41f236a815875b9b418ab042ec35a987a87a30
walter <walter@d90a1141-1c03-0410-82d0-813645769019>
2009-10-04 13:02:04 +0000

    app icon is compiled-in
    git-svn-id: file:///Users/walter/linux/walter/src/svnroot/trunk/mpflow@3289 d90a1141-1c03-0410-82d0-813645769019

--- src/include/app_icon.h
+++ src/include/app_icon.h
@@ -5,7 +5,10 @@
 #ifndef APP_ICON_H_WJ109
 #define APP_ICON_H_WJ109	1
 
+#define ICON_W	100
+#define ICON_H	100
 
+void set_app_icon(void);
 
 #endif	/* APP_ICON_H_WJ109 */
 
--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@
 #include "font.h"
 #include "text.h"
 #include "texture.h"
+#include "app_icon.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -146,36 +147,6 @@ int xres, yres;
 		info.info.x11.unlock_func();
 	} else
 		window_x = window_y = 0;
-}
-
-void set_app_icon(void) {
-SDL_Surface *icon, *img;
-int i, n;
-
-	if ((img = IMG_Load(APP_ICON)) == NULL) {
-		fprintf(stderr, "error: failed to load application icon image\n");
-		return;
-	}
-	if ((icon = SDL_CreateRGBSurfaceFrom(img->pixels, img->w, img->h, 32, img->w * 4, 0xff, 0xff00, 0xff0000, 0xff000000)) == NULL)
-		fprintf(stderr, "error: failed to create app icon surface\n");
-
-printf("unsigned char *app_icon[] = {\n\t");
-n = 0;
-for(i = 0; i < 100 * 100 * 4; i++) {
-	printf("0x%02x, ", ((unsigned char *)(img->pixels))[i]);
-
-	n++;
-	if (n >= 8) {
-		n = 0;
-		printf("\n\t");
-	}
-}
-printf("\n};\n\n");
-
-	SDL_WM_SetIcon(icon, NULL);
-	SDL_FreeSurface(icon);
-
-	SDL_FreeSurface(img);
 }
 
 /*
